9e3eb8066c3530c87bc350f5b6edd1ca3820bd51
Christopher Camacho <kilvari@gmail.com>
2022-08-08 14:30:03 -0400

    Bug fixes for Get-ImpervaCustomCertificate

--- Imperva Cloud WAF.ps1
+++ Imperva Cloud WAF.ps1
@@ -3,7 +3,7 @@
 #
 # CCamacho Template Driver Version: 202006101700
 #
-$Script:AdaptableAppVer = '202208081325'
+$Script:AdaptableAppVer = '202208081429'
 $Script:AdaptableAppDrv = "Imperva Cloud WAF"
 
 # Import Legacy Imperva sites?
@@ -185,7 +185,7 @@ function Extract-Certificate
         }
     }
 
-    $customCert = Get-ImpervaCustomCertificate -General $General -Website $siteId
+    $customCert = Get-ImpervaCustomCertificate -General $General -Website $siteInfo
 
     Write-VenDebugLog "Extracted Thumbprint and Serial Number - Returning control to Venafi"
     return @{ Result="Success"; Serial=$($customCert.serialNumber); Thumbprint=$($customCert.fingerprint) }
@@ -556,6 +556,8 @@ function Get-ImpervaCustomCertificate
         [Parameter(Mandatory)] [PSCustomObject] $Website
     )
 
+    Write-VenDebugLog "Called by $((Get-PSCallStack)[1].Command)"
+
     $siteId     = $Website.site_id
     $siteLegacy = $false
     $apiUrl     = "https://api.imperva.com/certificates/v3/certificates?extSiteId=$($siteId)&certType=CUSTOM_CERT"
@@ -575,14 +577,14 @@ function Get-ImpervaCustomCertificate
         throw $fatal
     }
 
-    if ($response.data.id -ne $siteId) {
-        $fatal = "Data Mismatch! Asked for site #$() but got results for site #$()..."
+    if ($response.data.extSiteId -ne $siteId) {
+        $fatal = "Data Mismatch! Asked for site #$($siteId) but got results for site #$($response.data.extSiteId)..."
         Write-VenDebugLog $fatal
         throw $fatal
     }
 
-    $siteSerial  = ($response.data.customCertificateDetails.serialNumber -replace ':','')
-    $siteThumb   = ($response.data.customCertificateDetails.fingerprint -replace 'SHA1 Fingerprint=','' -replace ':','')
+    $siteSerial  = $apiSerial = ($response.data.customCertificateDetails.serialNumber -replace ':','')
+    $siteThumb   = $apiThumb  = ($response.data.customCertificateDetails.fingerprint -replace 'SHA1 Fingerprint=','' -replace ':','')
     $certExpires = Convert-ImpervaTimestamp "$($response.data.expirationDate)"
 
     # Always attempt to pull the public certificate from the WAF front-end
@@ -591,6 +593,7 @@ function Get-ImpervaCustomCertificate
 
     if (($null -eq $siteSerial) -or ($siteSerial -eq '')) {
         $siteLegacy = $true
+        Write-VenDebugLog 'Legacy website: Certificate data not available via API'
         $siteSerial = $siteCert.X509.SerialNumber.TrimStart('0')
         $siteThumb  = $siteCert.X509.Thumbprint.TrimStart('0')
     }
@@ -607,21 +610,23 @@ function Get-ImpervaCustomCertificate
 
     if ($null -ne $siteCert) {
         Write-VenDebugLog "Certificate '$($siteCert.X509.GetNameInfo(0,$false))' issued by '$($siteCert.X509.GetNameInfo(0,$true))'"
-        if (($null -ne $response.data.customCertificateDetails.serialNumber) -and (($siteCert.X509.SerialNumber.TrimStart('0') -ne $response.data.customCertificateDetails.serialNumber.TrimStart('0')) -or ($siteCert.X509.Thumbprint.TrimStart('0') -ne $response.data.customCertificateDetails.fingerprint.TrimStart('0')))) {
-            # This is a pretty annoying bug...
-            #
-            # If you use a certificate on multiple WAF sites (I.E. SANs or wildcards)
-            # then you should know that every site tracks details based on a site-specific upload
-            # Every site retains its own certificate details...
-            # ...BUT uploading a shared certificate on any site actually affects all the others on
-            # the backend. To make the saved data validate, you have to upload the certificate to
-            # each and every site. This bug also can be seen in the web UI.
-            #
-            # This is silly, BUT we should at least flag/note this in the debug logs.
-            Write-VenDebugLog "WARNING: Imperva Bug - Certificate Mismatch in API vs WAF - Validation will FAIL"
-            Write-VenDebugLog "\\-- API: Serial=$($siteSerial), Thumbprint=$($siteThumb)"
-            Write-VenDebugLog "\\-- WAF: Serial=$($siteCert.X509.SerialNumber), Thumbprint=$($siteCert.X509.Thumbprint)"
-            Write-VenDebugLog "\\-- Reinstall certificate via API or WebUI to fix this issue"
+        if ($null -ne $response.data.customCertificateDetails.serialNumber) {
+            if (($siteCert.X509.SerialNumber.TrimStart('0') -ne $apiSerial) -or ($siteCert.X509.Thumbprint.TrimStart('0') -ne $apiThumb)) {
+                # This is a pretty annoying bug...
+                #
+                # If you use a certificate on multiple WAF sites (I.E. SANs or wildcards)
+                # then you should know that every site tracks details based on a site-specific upload
+                # Every site retains its own certificate details...
+                # ...BUT uploading a shared certificate on any site actually affects all the others on
+                # the backend. To make the saved data validate, you have to upload the certificate to
+                # each and every site. This bug also can be seen in the web UI.
+                #
+                # This is silly, BUT we should at least flag/note this in the debug logs.
+                Write-VenDebugLog "WARNING: Imperva Bug - Certificate Mismatch in API vs WAF - Validation will FAIL"
+                Write-VenDebugLog "\\-- API: Serial=$($response.data.customCertificateDetails.serialNumber), Thumbprint=$($response.data.customCertificateDetails.fingerprint)"
+                Write-VenDebugLog "\\-- WAF: Serial=$($siteCert.X509.SerialNumber), Thumbprint=$($siteCert.X509.Thumbprint)"
+                Write-VenDebugLog "\\-- Reinstall certificate via API or WebUI to fix this issue"
+            }
         } # UI/WAF mismatch warning
     }
 

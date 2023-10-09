9fae4120d6066e643750aa0d818580bdc532b12e
Christopher Camacho <kilvari@gmail.com>
2023-10-09 19:00:13 -0400

    updated

--- Imperva Cloud WAF.ps1
+++ Imperva Cloud WAF.ps1
@@ -3,15 +3,24 @@
 #
 # CCamacho Template Driver Version: 202212281725
 #
-$Script:AdaptableAppVer = '202306281633'
-$Script:AdaptableAppDrv = "Imperva Cloud WAF"
+$Script:AdaptableAppVer = '202308071848'
+$Script:AdaptableAppDrv = 'Imperva Cloud WAF'
 
-# Import Legacy Imperva sites?
+# Global driver configurations that can't be setup any other way
+
+# These can enable/disable functionality for the discovery driver
 #
-# We now spoof the API validation using the workaround mechanism
-# employed to enable discovery by this driver. It's ugly to look
-# at (if you're a programmer), but it makes this function well.
-$Script:ImportLegacy    = $true
+# SubAccounts: Scan for new subaccounts and add them to the policy tree?
+# AddPolicies: Place new subaccount devices in a new policy folder?
+
+$Script:DiscoveryOptions = @{
+    SubAccounts = $true
+    AddPolicies = $true
+}
+
+# If the website has been moved between subaccounts on Imperva then attempt to just
+# move the application to the correct device rather than throwing a validation error
+$Script:MoveWebSites = $true
 
 <#
 
@@ -103,14 +112,14 @@ function Install-Certificate
     }
 
     if ($siteInfo.res -ne 0) {
-        $apiError=Get-ImpervaErrorMessage -Code $siteInfo.res
-        Write-VenDebugLog "API error: $($apiError): $($siteInfo.debug_info.Error)"
+        $apiError = "API error $($siteInfo.res): $($siteInfo.debug_info.Error)"
+        Write-VenDebugLog $apiError
         if ($siteInfo.res -eq 3015) {
             Write-VenDebugLog "Temporary Error - Returning control to Venafi (Resume Later)"
             return @{ Result="ResumeLater"; }
         }
         Write-VenDebugLog "Install FAILED - Returning control to Venafi"
-        throw "API error: $($apiError): $($siteInfo.debug_info.Error)"
+        throw $apiError
     }
 
     $certExpires=Convert-ImpervaTimestamp $siteInfo.debug_info.details.expirationDate
@@ -188,9 +197,7 @@ function Extract-Certificate
     }
 
     if ($siteInfo.res -ne 0) {
-        $apiError=Get-ImpervaErrorMessage -Code $siteInfo.res
-        Write-VenDebugLog "API error: $($apiError)"
-        throw $apiError
+        "API error $($siteInfo.res): $($siteInfo.debug_info.Error)" | Write-VenDebugLog -ThrowException
     }
 
     if ($wafAccount) {
@@ -255,60 +262,77 @@ function Discover-Certificates
     )
  
     $started=Get-Date
-
     Initialize-VenDebugLog -General $General
-
     [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-    $apiUrl ="https://my.imperva.com/api/prov/v1/sites/list"
+
+
+    # Important constants for the discovery function
+    $BasicPost = @{
+        General = $General
+        Method  = 'Post'
+    }
+    $DiscoveryApi = $BasicPost + @{
+        Uri     = 'https://my.imperva.com/api/prov/v1/sites/list'
+    }
+
+    Write-VenDebugLog "General: $($General | ConvertTo-Json | Out-String)"
 
     if (($General.HostAddress -eq '') -or ($General.HostAddress -eq '*')) {
-        $wafAccount='*'
+        $wafAccount = '*'
+        $wafSearch  = ''
+    } else {
+        $wafAccount = $General.HostAddress
+        $wafSearch  = "account_id=$($wafAccount)&"
+        # Discover sub-accounts if setup correctly
+        if ($Script:DiscoveryOptions.SubAccounts) {
+            $ImpervaUri      = [uri]"https://my.imperva.com/api/prov/v1/accounts/listSubAccounts?account_id=$($wafAccount)"
+            $ImpervaResponse = Invoke-ImpervaRestMethod @BasicPost -Uri $ImpervaUri
+            if ($ImpervaResponse.res -eq 0) {
+                Write-VenDebugLog 'Beginning sub-account discovery'
+                # This is a parent/main account - process subaccounts
+                foreach ($subAccount in $ImpervaResponse.resultList) {
+                    Write-VenDebugLog "Processing sub-account #$($subAccount.sub_account_id): $($subAccount.sub_account_name)"
+                    if ($subAccount.parent_id -eq $wafAccount) {
+                        # valid - search for existing device - no match then create a new one
+                    } else {
+                        Write-VenDebugLog "ERROR: Parent Account is '$($subAccount.parent_id)', expected '$($wafAccount)'... Skipping!"
+                    }
+                }
+                Write-VenDebugLog 'Sub-account discovery complete'
+            }
+        }
     }
-    else {
-        $wafAccount=$General.HostAddress
-    }
+    $accountRegex = "^$($WafAccount)$"
 
-    # How many sites to pull per API call - 10 is a reasonably small/quick chunk
-    # maximum supported by Imperva is 100
-    $psize=10
+    # How many sites to pull per API call - maximum supported by Imperva is 100
+    $psize=25
 
     # Initialize counters, arrays, lists
-    $page=$siteCount=$skipped=$sslSites=$sslFree=$sslLegacyIgnored=$sslLegacyAdded=$sslDiscovered=$inactiveSites=0
+    $page=$siteCount=$sslSites=$sslFree=$sslLegacyAdded=$sslDiscovered=$inactiveSites=0
     $siteList=@()
 
     do {
-        $batch=0
-        if ($WafAccount -eq '*') {
-            $apiBody="page_size=$($psize)&page_num=$($page)"
-        }
-        else {
-            $apiBody="account_id=$($WafAccount)&page_size=$($psize)&page_num=$($page)"
-        }
+        $batch   = 0
+        $apiBody = "$($wafSearch)page_size=$($psize)&page_num=$($page)"
 
         try {
             Write-VenDebugLog "Requesting sites $(($page*$psize)+1) through $(($page+1)*($psize))"
-            $siteInfo=Invoke-ImpervaRestMethod -General $General -Method Post -Uri $apiUrl -Body $apiBody
-        }
-        catch {
-            Write-VenDebugLog "API method failure: $($siteInfo)"
-            throw("API method failure: $($siteInfo)")
+            $siteInfo = Invoke-ImpervaRestMethod @DiscoveryApi -Body $apiBody
+        } catch {
+            "API method failure: $($siteInfo)" | Write-VenDebugLog -ThrowException
         }
 
         foreach($site in $siteInfo.sites) {
             $batch++
             $siteCount++
-            $accountRegex="^$($WafAccount)$"
             if ($site.account_id -notmatch $accountRegex) {
-                # this site is in a sub-account ... skipping
-                $skipped++
-                Write-VenDebugLog "Ignored: $($site.display_name) in sub-account #$($site.account_id)"
-            }
-            elseif ($site.active -ne 'active') {
+                # Skip site in sub-account when searching parent/root
+                #Write-VenDebugLog "Ignored: $($site.display_name) in sub-account #$($site.account_id)"
+            } elseif ($site.active -ne 'active') {
                 # This site is NOT active...
                 $inactiveSites++
                 Write-VenDebugLog "Ignored: $($site.display_name) is inactive"
-            }
-            else {
+            } else {
                 $customCert = Get-ImpervaCustomCertificate -General $General -Website $site
                 if ($customCert) {
                     $wafHost = $customCert.wafHost
@@ -316,25 +340,20 @@ function Discover-Certificates
                     if ($customCert.legacy) {
                         $sslLegacyAdded++
                         Write-VenDebugLog "Discovered: [$($site.display_name)] (Legacy Site #$($site.site_id) at $($wafHost))"
-                    }
-                    else {
+                    } else {
                         $sslDiscovered++
                         Write-VenDebugLog "Discovered: [$($site.display_name)] (Site #$($site.site_id) at $($wafHost))"
                     }
                     $wafSite = @{
-                        Name = "$($site.display_name)"      # Name of the Adaptable Application object
-                        PEM = $customCert.certificate.PEM   # Formatted PEM version of the public certificate
+                        Name = $site.display_name         # Name of the Adaptable Application object
+                        PEM = $customCert.certificate.PEM # Formatted PEM version of the public certificate
                         # Venafi currently fails when trying to validate this way due to weak SNI support
-                        ValidationAddress = $wafHost        # FQDN of Imperva WAF Front-End
-                        ValidationPort = 443                # TCP port (Currently hard coded to 443)
-                        Attributes = @{
-                            "Text Field 1" = "$($site.site_id)"
-#                           "Certificate Name" = ""
-                        }
+                        ValidationAddress = $wafHost      # FQDN of Imperva WAF Front-End
+                        ValidationPort = 443              # TCP port (Currently hard coded to 443)
+                        Attributes = @{ 'Text Field 1' = $site.site_id }
                     } # Venafi Application definition for the current site
                     $siteList += $wafSite
-                } # custom certificate was retrieved
-                else {
+                } else { # custom certificate was retrieved
                     $sslFree++
                     Write-VenDebugLog "Ignored: $($site.display_name) is unencrypted"
                 } # WAF on an HTTP only site.?! Eeew...
@@ -350,17 +369,14 @@ function Discover-Certificates
         }
         Write-VenDebugLog $logMessage
     }
-    if ($sslLegacyIgnored+$sslFree+$inactiveSites+$skipped -gt 0) {
-        $logMessage = "Ignored $($sslLegacyIgnored+$sslFree+$inactiveSites+$skipped) sites ("
+    if ($sslFree+$inactiveSites -gt 0) {
+        $logMessage = "Ignored $($sslFree+$inactiveSites) sites ("
         if ($sslFree -gt 0)          { $logMessage += "$($sslFree) unencrypted, " }
-        if ($sslLegacyIgnored -gt 0) { $logMessage += "$($sslLegacyIgnored) legacy, " }
         if ($inactiveSites -gt 0)    { $logMessage += "$($inactiveSites) inactive, " }
-        if ($skipped -gt 0)          { $logMessage += "$($skipped) sub-account sites" }
         Write-VenDebugLog "$($logMessage.TrimEnd(', ')))"
     }
 
-    $finished = Get-Date
-    $runtime = New-TimeSpan -Start $started -End $finished
+    $runtime = New-TimeSpan -Start $started -End (Get-Date)
     Write-VenDebugLog "Scanned $($siteCount) sites (Runtime $($runtime)) - Returning control to Venafi"
  
     return @{ Result='Success'; Applications=$siteList }
@@ -375,8 +391,14 @@ function Discover-Certificates
 function Write-VenDebugLog
 {
     Param(
-        [Parameter(Position=0, Mandatory)][string]$LogMessage,
-        [switch]$NoFunctionTag
+        [Parameter(Position=0, Mandatory)]
+        [string] $LogMessage,
+
+        [Parameter()]
+        [switch] $ThrowException,
+
+        [Parameter()]
+        [switch] $NoFunctionTag
     )
 
     filter Add-TS {"$(Get-Date -Format o): $_"}
@@ -386,13 +408,17 @@ function Write-VenDebugLog
 
     if ($NoFunctionTag.IsPresent) {
         $taggedLog = $LogMessage
-    }
-    else {
+    } else {
         $taggedLog = "[$((Get-PSCallStack)[1].Command)] $($LogMessage)"
     }
 
     # write the message to the debug file
     Write-Output "$($taggedLog)" | Add-TS | Add-Content -Path $Script:venDebugFile
+
+    # throw the message as an exception, if requested
+    if ($ThrowException.IsPresent) {
+        throw $LogMessage
+    }
 }
 
 function Initialize-VenDebugLog
@@ -438,11 +464,19 @@ function Initialize-VenDebugLog
 function Invoke-ImpervaRestMethod
 {
 	Param(
-        [Parameter(Mandatory)] [System.Collections.Hashtable] $General,
-		[Parameter(Mandatory)] [Uri] $Uri,
-		[Parameter(Mandatory)] [Microsoft.PowerShell.Commands.WebRequestMethod] $Method,
+        [Parameter(Mandatory)]
+        [System.Collections.Hashtable] $General,
+
+		[Parameter(Mandatory)]
+        [Uri] $Uri,
+
+		[Parameter(Mandatory)]
+        [Microsoft.PowerShell.Commands.WebRequestMethod] $Method,
+
 		[System.Object] $Body,
+
 		[string] $ContentType = 'application/x-www-form-urlencoded',
+
 		[int] $TimeoutSec
 	)
 
@@ -452,19 +486,28 @@ function Invoke-ImpervaRestMethod
         'x-API-Id'  = $General.UserName;
         'x-API-Key' = $General.UserPass
     }
+    $ImpervaRestApiCall = @{
+        Uri         = $Uri
+        Method      = $Method
+        Headers     = $apiAuth
+        ContentType = $ContentType
+    }
+    if ($Body)       { $ImpervaRestApiCall.Body       = $Body       }
+    if ($TimeoutSec) { $ImpervaRestApiCall.TimeoutSec = $TimeoutSec }
 
     $Attempts=3
     $i=0
     do {
         try {
-            $response = Invoke-RestMethod -Uri $Uri -Method $Method -Headers $apiAuth -Body $Body -ContentType $ContentType -TimeoutSec $TimeoutSec
-        }
-        catch {
-            Write-VenDebugLog "REST call failed to '$($Uri)'"
+            $response = Invoke-RestMethod @ImpervaRestApiCall
+        } catch {
+            Write-VenDebugLog "REST call failed to '$($Uri)': $($_)"
             throw $_
         }
+
         # return response upon success, otherwise retry
         if ($response.res -eq 0) { return $response }
+
         $i++
         $wait = Get-Random -Minimum ($i+1) -Maximum ($i*3)
         Write-VenDebugLog "Attempt #$($i) failed ($($response.debug_info.Error)) - sleeping for $($wait) seconds"
@@ -477,38 +520,13 @@ function Invoke-ImpervaRestMethod
 
 function Convert-ImpervaTimestamp
 {
-    Param( [Parameter(Position=0, Mandatory)][String]$Timestamp )
+    Param(
+        [Parameter(Position=0, ValueFromPipeline, Mandatory)]
+        [String]$Timestamp
+    )
 
-    # Convert Imperva microseconds to seconds
-    $epoch=$Timestamp/1000
-
-    ([DateTime]'1/1/1970Z').AddSeconds($epoch)
-}
-
-function Get-ImpervaErrorMessage
-{
-    Param( [Parameter(Position=0, Mandatory)][string]$Code )
-
-    Write-VenDebugLog "Called by $((Get-PSCallStack)[1].Command)"
-
-    $ImpervaError = @{
-        '1'='The server has encountered an unexpected error'
-        '2'='Input missing or incorrect'
-        '4'='The server is not available or reached a time-out while processing the operation'
-        '3015'='Internal error, please try again'
-        '4205'='The site does not support SSL (HTTPS)'
-        '9403'='The specified account is unknown or client is not authorized to operate on it'
-        '9411'='Authentication parameters missing or incorrect'
-        '9413'='The specified site is unknown or client is not authorized to operate on it'
-        '9414'="Feature is not available on account's plan"
-        '9415'='The requested operation is not allowed'
-    }
-
-    if ($null -eq $ImpervaError[$Code]) {
-        return "Unknown Imperva Error Code: $($Code)"
-    }
-
-    $ImpervaError[$Code]
+    # Convert microseconds to seconds and add to 'epoch' date
+    ([DateTime]'1/1/1970Z').AddSeconds($Timestamp/1000)
 }
 
 function Get-ImpervaCustomCertificate
